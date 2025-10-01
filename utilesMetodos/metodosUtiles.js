cbd3b331110f3fbde4869ad208d09250eb61f3ef
Carlos Medina <carlosmbinf@gmail.com>
2025-09-30 21:11:47 -0300

    Revert "Eliminar métodos de inicio de sesión con Google y limpiar código relacionado"
    This reverts commit bbb4bcc5a635a1848580f3072df5434dc37949d0.

---
+++ utilesMetodos/metodosUtiles.js
@@ -0,0 +1,74 @@
+import Meteor from '@meteorrn/core';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { Alert } from 'react-native';
+
+/**
+ * Login with Google using Meteor Accounts and React Native
+ * @param configuration Options for the configure method of GoogleSignin.
+ * @param callback function to see if there is error
+ * @returns {Promise<void> | Promise.Promise}
+ */
+const loginWithGoogle = async function(configuration, callback) {
+	try {
+		await GoogleSignin.configure(configuration);
+		await GoogleSignin.hasPlayServices({
+			showPlayServicesUpdateDialog: true
+		});
+		let userInfo;
+		var isSignedIn = await GoogleSignin.isSignedIn();
+
+
+		if (!isSignedIn) {
+			userInfo = await GoogleSignin.signIn();
+			if (!userInfo) {
+				callback({ reason: 'Algo salió mal al obtener la información del usuario', details: { userInfo } });
+				return;
+			}
+		} else {
+			userInfo = await GoogleSignin.signInSilently();
+			if (!userInfo) {
+				callback({ reason: 'Algo salió mal al obtener la información del usuario', details: { userInfo } });
+				return;
+			}
+		}
+		const tokens = await GoogleSignin.getTokens();
+		await Meteor._startLoggingIn();
+		await Meteor.call(
+			'login',
+			{
+				googleSignIn: true,
+				accessToken: tokens.accessToken,
+				refreshToken: undefined,
+				idToken: tokens.idToken,
+				serverAuthCode: userInfo.serverAuthCode,
+				email: userInfo.user.email,
+				imageUrl: userInfo.user.photo,
+				userId: userInfo.user.id
+			},
+			(error, response) => {
+				if (error) {
+					GoogleSignin.revokeAccess();
+					GoogleSignin.signOut();
+				}
+				Meteor._endLoggingIn();
+				Meteor._handleLoginCallback(error, response);
+				typeof callback == 'function' && callback(error);
+			}
+		);
+	} catch (error) {
+		callback({ reason: 'No se pudo iniciar session con google, por favor inicie session con Usuario y contraseña', details: { error } });
+	}
+};
+
+const logoutFromGoogle = function() {
+		Meteor.logout((error) => {
+			if (!error) {
+				GoogleSignin.revokeAccess();
+				GoogleSignin.signOut();
+			}else{
+				console.error('Error during Google logout:', error);
+			}
+		});
+};
+
+export { loginWithGoogle, logoutFromGoogle };
