e25f5a645a3a4ff33268d748f402df3915d392f7
Carlos Medina <carlosmbinf@gmail.com>
2025-10-08 10:37:02 -0300

    Actualizar versiones de SDK y NDK en build.gradle; agregar archivos generados por CMake

---
+++ android/app/.cxx/Debug/t3e5h671/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Krly/.gradle/caches/8.8/transforms/2b2c08b78f61be1aff6d584e740d7456/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Krly/.gradle/caches/8.8/transforms/2b2c08b78f61be1aff6d584e740d7456/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/Debug/t3e5h671/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Krly/.gradle/caches/8.8/transforms/588ac5417930ba27f329d5472941c9ad/transformed/hermes-android-0.75.3-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Krly/.gradle/caches/8.8/transforms/588ac5417930ba27f329d5472941c9ad/transformed/hermes-android-0.75.3-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
