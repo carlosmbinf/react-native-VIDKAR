2d6f8d017722b1293256c78fde09a91464d1120d
Carlos Medina <carlosmbinf@gmail.com>
2025-12-01 08:40:00 -0500

    feat: agregar estructura inicial de la aplicación Watch con ContentView y archivos de configuración

---
+++ ios/vidkar Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vidkar Watch App
+//
+//  Created by user285589 on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
