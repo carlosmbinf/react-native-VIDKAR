3aae0793d3f70b3129111ad88b51b485bb1f4df2
carlosmbinf <53983616+carlosmbinf@users.noreply.github.com>
2026-01-13 12:58:15 -0300

    Update marketing version format in ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -92,7 +92,7 @@ xcrun agvtool new-version -all $CI_BUILD_NUMBER
 echo "CFBundleVersion set to $CI_BUILD_NUMBER"
 
 # 2️⃣ Actualizar la versión de marketing (CFBundleShortVersionString)
-MARKETING_VERSION="1.0.$CI_BUILD_NUMBER"  # <- ajusta el "1.0" si quieres otro prefijo
+MARKETING_VERSION="1.$CI_BUILD_NUMBER"  # <- ajusta el "1.0" si quieres otro prefijo
 xcrun agvtool new-marketing-version $MARKETING_VERSION
 echo "CFBundleShortVersionString set to $MARKETING_VERSION"
 
