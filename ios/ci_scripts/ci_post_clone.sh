20ca80a92e99283fb01ccd7f458e9f59ce6a8c91
carlosmbinf <53983616+carlosmbinf@users.noreply.github.com>
2026-02-09 15:29:51 +0000

    fix: actualizar versión de Node a 22 en el script de CI

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -12,8 +12,8 @@ CI_BUILD_NUMBER=$(( ${original} + 60 ))
 echo "Build Number original: $original -> Build Number ajustado: $CI_BUILD_NUMBER"
 
 # Instalar Node (versión estable para RN), Yarn y CocoaPods
-brew install node@18 yarn cocoapods
-brew link --overwrite node@18
+brew install node@22 yarn cocoapods
+brew link --overwrite node@22
 
 # Ir a la raíz del repo (donde está package.json)
 cd "$CI_PRIMARY_REPOSITORY_PATH"
